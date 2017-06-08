d7547dfc2de7dcc32c6b9c2cb9d408d1b69e19db
Joaquín R <globojorro@gmail.com>
2017-06-08 00:36:22 -0500

    friendlier code

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Very simple Arduino code that configures it as an I2C slave that blinks the buil
 
 This program was tested with receiving commands from [hello-i2c-pi](https://github.com/saibatizoku/hello-i2c-pi), as the master.
 
+To run this program, you need to have the "Wire" library in your Arduino installation, then you can install by cloning this repository into your corresponding Arduino directory. Usually something like:
+
+```
+cd ~/Arduino
+git clone https://github.com/saibatizoku/arduino-i2c-slave-blinker.git
+```
+
+Once you have the code, simply upload it to your favourite board and hook it up to a proper master.
--- slave_blinker.ino
+++ slave_blinker.ino
@@ -9,29 +9,29 @@
 // Turns the built-in LED on and off upon command.
 
 // Created 07 June 2017
-
 // This example code is in the public domain.
 
-
 #include <Wire.h>
-byte SLAVE_ADDRESS = 8;
-byte COMMAND_LED_ON = 0x01;
-byte COMMAND_LED_OFF = 0x00;
-bool LED_STATUS;
+
+bool BLINKER_STATUS;
+byte COMMAND_BLINKER_ON = 0x01;
+byte COMMAND_BLINKER_OFF = 0x00;
 int LED_TIME_LOW = 680;
 int LED_TIME_HIGH = 320;
+byte SLAVE_I2C_ADDRESS = 0x08;
 
 void setup() {
-  LED_STATUS = true;
-  Wire.begin(SLAVE_ADDRESS);                // join i2c bus with address #8
-  Wire.onReceive(receiveEvent); // register event
-  Serial.begin(9600);           // start serial for output
+  BLINKER_STATUS = true;
+  Wire.begin(SLAVE_I2C_ADDRESS);  // join i2c bus with address #8
+  Wire.onReceive(receiveEvent);   // register event
+  Serial.begin(9600);             // start serial for output
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+// the loop function runs over and over again forever
 void loop() {
-  if (LED_STATUS==true) {
-    led_loop();
+  if (BLINKER_STATUS==true) {
+    blink_led();
   } else {
     delay(100);
   }
@@ -41,16 +41,15 @@ void loop() {
 // this function is registered as an event, see setup()
 void receiveEvent(int howMany) {
   byte x = Wire.read();    // receive byte as an integer
-  Serial.println(x);         // print the integer
-  if (x == COMMAND_LED_ON) {
-    LED_STATUS = true;
-  } else if(x == COMMAND_LED_OFF) {
-    LED_STATUS = false;
+  Serial.println(x);       // print the integer
+  if (x == COMMAND_BLINKER_ON) {
+    BLINKER_STATUS = true;
+  } else if(x == COMMAND_BLINKER_OFF) {
+    BLINKER_STATUS = false;
   }
 }
 
-// the loop function runs over and over again forever
-void led_loop() {
+void blink_led() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(LED_TIME_HIGH);              // wait for LED_TIME_HIGH in millis
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
